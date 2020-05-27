c4cbc965ffa471c89e6bce847fa3790ef5ee671c
amb9559 <65776755+amb9559@users.noreply.github.com>
2020-05-27 22:24:25 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-int main() {
-    int i, n, f = 0, s = 1, nt;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("Fibonacci Series: ");
-
-    for (i = 1; i <= n; ++i) {
-        printf("%d, ", f);
-        nt = f + s;
-        f = s;
-        s = nt;
+#include<stdio.h>
+int main()
+{   
+ int i,a = 0, b = 1, c;
+    printf("fibonacci series: ");
+    for(i=0;i<=5;i++)
+    {
+        printf("%d,",a);
+        c = a + b;
+        a = b;
+        b = c;
     }
-
-    return 0;
 }
